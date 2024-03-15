1341154c41ded28e77ed5959dd555052ed0cb1fd
Nazrin <210138054@aston.ac.uk>
2024-03-15 02:56:08 +0000

    Final Grid_search method

--- ur5eWorld_new/controllers/my_controllerKINPY/my_controllerKINPY.py
+++ ur5eWorld_new/controllers/my_controllerKINPY/my_controllerKINPY.py
@@ -56,20 +56,19 @@ class UR5e(Robot):
         self.height = self.camera.getHeight()
         self.width = self.camera.getWidth()
         self.camera.recognitionEnable(TIME_STEP)
- 
-                 
+        
         print('robot initialised.')
         
     def create_urdf(self, urdf_fn='C:/Users/Nazrin/Webots_ur5eSimulation/UR5e.urdf'):
-        with open(urdf_fn, "w") as file:
-            file.write(self.getUrdf())
+        #with open(urdf_fn, "w") as file:
+            #file.write(self.getUrdf())
             #load chain
         self.ur5e = Chain.from_urdf_file(urdf_fn,
-                            active_links_mask=[0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1])
-        print(f'saved urdf to {urdf_fn} with joint pos {self.joint_pos()}')
-        
-        # Print out the end effector link
-        end_effector_link = self.ur5e.links[-1]  # Assuming the last link is the end effector (wrist)
+                            active_links_mask=[0, 1, 1, 1, 1, 1, 0])
+        print(f'initial joint pos {self.joint_pos()}')
+
+        # Print out end effector link
+        end_effector_link = self.ur5e.links[-1] 
         print(f"End Effector Link: {end_effector_link.name}")
         
     def joint_pos(self):
@@ -83,25 +82,24 @@ class UR5e(Robot):
             self.step()
 
     def move_end_effector(self, x, y, z):
-        #returns 2-dimensional array
-        target_orientation = [0, 1, 0]
-        initial_position = [0] + [m.getPositionSensor().getValue() for m in self.motors] + [0, 1, 1, -1]
-        # Compute IK with position and orientation
+        #set orientation and initial position
+        target_orientation = [0, 0, -1]
+        initial_position = [0] + [m.getPositionSensor().getValue() for m in self.motors]
+        #Compute IK with position and orientation
         ikResults = self.ur5e.inverse_kinematics([x, y, z], target_orientation=target_orientation, orientation_mode="Y", max_iter=IKPY_MAX_ITERATIONS, initial_position=initial_position)
-        #final positions and orientations of robot
+        #final positions and orientation 
         position = self.ur5e.forward_kinematics(ikResults)[:3, 3]
-        orientation = self.ur5e.forward_kinematics(ikResults)[:3, 1]
-        #print("target orientation", target_orientation, orientation)
+        orientation = self.ur5e.forward_kinematics(ikResults)[:3, 1]   
+
         if ikResults is not None:
             for i, motor in enumerate(self.motors):
                 motor.setPosition(ikResults[i+1])
         for step in range(MAX_STEPS):
-            self.step(TIME_STEP)           
-           
+            self.step(TIME_STEP) 
            
     def grasp(self):
         print("grasping...")
-        self.move_to_joint_pos([0.0, -0.85, 1.30, -1.57, -1.57, 0.0])
+        self.move_to_joint_pos([0.0, -1.06, 1.57, -1.57, -1.57, 0.0])
         for m in self.grippers:
             m.setPosition(0.96)
             m.setVelocity(speed)
@@ -152,7 +150,6 @@ class UR5e(Robot):
 
     def rotation_search(self):
         print("performing rotational search...")
-       
         # Move to the left position
         target_joint_pos_left = [0, -1.57, 1.57, -1.57, -0.6, 0.0]
         self.move_to_joint_pos(target_joint_pos_left)
@@ -183,39 +180,39 @@ class UR5e(Robot):
         print("implementing grid search...")
         x = 0.37
         y = 0.5
-        z = 0.3
+        z = 0.4
         
         step_size = 0.3      
         step_up = 0.2
         step = 3
-
-        self.move_end_effector(x,y,z)
-            
+        
+        self.move_end_effector(x,y,z)  
+        
         for i in range(2):
             for k in range(step):
                 y -= step_size
                 self.move_end_effector(x, y, z)
-                #if self.object_detection():
-                    #self.grasp()
-                    #return    
+                if self.object_detection():
+                    self.grasp()
+                    return    
             for j in range(1):
                 x += step_up
                 self.move_end_effector(x,y,z) 
-                #if self.object_detection():
-                    #self.grasp()
-                    #return       
+                if self.object_detection():
+                    self.grasp()
+                    return       
             for m in range(step): 
                 y += step_size
                 self.move_end_effector(x,y,z)
-                #if self.object_detection():
-                    #self.grasp()
-                    #return      
+                if self.object_detection():
+                    self.grasp()
+                    return      
             for n in range(1):
                 x += step_up
                 self.move_end_effector(x,y,z)   
-                #if self.object_detection():
-                    #self.grasp()
-                    #return
+                if self.object_detection():
+                    self.grasp()
+                    return
                     
                          
 if __name__ == '__main__':        
