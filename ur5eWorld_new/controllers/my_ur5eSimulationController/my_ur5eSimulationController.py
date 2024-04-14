b6648049e5980ad5969a9d04cc31e4169e444caa
Nazrin <210138054@aston.ac.uk>
2024-04-14 23:23:48 +0100

    adding comments, code cleanup

--- ur5eWorld_new/controllers/my_ur5eSimulationController/my_ur5eSimulationController.py
+++ ur5eWorld_new/controllers/my_ur5eSimulationController/my_ur5eSimulationController.py
@@ -6,6 +6,7 @@ from ikpy.chain import Chain
 from time import sleep
 import math
 
+# Constants
 TIME_STEP = 32
 MAX_STEPS = 100
 IKPY_MAX_ITERATIONS = 4
@@ -15,6 +16,7 @@ class UR5e(Robot):
     def __init__(self):
         super().__init__()
         
+        #Position sensors
         position_sensors = [
             self.getDevice("shoulder_pan_joint_sensor"),
             self.getDevice("shoulder_lift_joint_sensor"),
@@ -24,6 +26,7 @@ class UR5e(Robot):
             self.getDevice("wrist_3_joint_sensor")
         ]
         
+        # Motors
         self.motors = [
             self.getDevice("shoulder_pan_joint"),
             self.getDevice("shoulder_lift_joint"),
@@ -33,6 +36,7 @@ class UR5e(Robot):
             self.getDevice("wrist_3_joint")
         ]
         
+        # Grippers
         self.grippers = [
             self.getDevice("finger_middle_joint_1"),
             self.getDevice("finger_middle_joint_2"),
@@ -45,7 +49,7 @@ class UR5e(Robot):
             self.getDevice("finger_1_joint_3"),
         ]
         
-        #Set positions and velocity
+        # Set positions and velocity
         for motor in self.motors:
             motor.getPositionSensor().enable(TIME_STEP)
             motor.setVelocity(speed)
@@ -56,39 +60,30 @@ class UR5e(Robot):
 
         print('robot initialised.')
         
+        # Create and load chain
     def create_urdf(self, urdf_fn='C:/Users/Nazrin/Webots_ur5eSimulation/UR5e.urdf'):
-        # Load chain
         self.ur5e = Chain.from_urdf_file(urdf_fn, active_links_mask=[0, 1, 1, 1, 1, 1, 0])
-        
-        print(f'Number of links: {len(self.ur5e.links)}')
-        print('List of links:')
-        for link in self.ur5e.links:
-            print(link.name)
-            
         print(f'Initial joint positions: {self.joint_pos()}')
-    
-        # Print out end effector link
-        end_effector_link = self.ur5e.links[-1]
-        print(f"End Effector Link: {end_effector_link.name}")
-    
-        
+
+    # Get current joint positions
     def joint_pos(self):
         return np.asarray([m.getPositionSensor().getValue() for m in self.motors])
         
+    # Move to joint position function
     def move_to_joint_pos(self, target_joint_pos, timeout=2):
         for pos, motor in zip(target_joint_pos, self.motors):
             motor.setPosition(pos)
-        #step through simulation using MAX_STEPS
         for step in range(timeout * 1000 // TIME_STEP):
             self.step()
 
+    # Move end effector function
     def move_end_effector(self, x, y, z):
-        #set orientation and initial position
+        # Set orientation and initial position
         target_orientation = [0, 0, -1]
         initial_position = [0] + [m.getPositionSensor().getValue() for m in self.motors]
-        #Compute IK with position and orientation
+        # Compute IK with position and orientation
         ikResults = self.ur5e.inverse_kinematics([x, y, z], target_orientation=target_orientation, orientation_mode="Y", max_iter=IKPY_MAX_ITERATIONS, initial_position=initial_position)
-        #final positions and orientation 
+        # Final positions and orientation 
         position = self.ur5e.forward_kinematics(ikResults)[:3, 3]
         orientation = self.ur5e.forward_kinematics(ikResults)[:3, 1]   
 
@@ -97,76 +92,53 @@ class UR5e(Robot):
                 motor.setPosition(ikResults[i+1])
         for step in range(MAX_STEPS):
             self.step(TIME_STEP) 
-            
-    def grasp(self):
-        print("grasping...")
-
-
-    def release(self):
-        print("releasing...")
-        for m in self.grippers:
-            m.setPosition(0.1)
-            m.setVelocity(speed)
-        for step in range(MAX_STEPS):
-            self.step(TIME_STEP)
-            
+           
+    # Get imsge function
     def get_image(self):
         print("capturing image...")
+        # Get image array
         img = self.camera.getImageArray()  
         img = np.array(img)    
-        #print('image shape', img.shape)
         img_height, img_width, channels = img.shape          
         img = np.array(img, dtype=np.uint8)
         img = np.reshape(img, (img_height, img_width, channels))        
         img = cv2.cvtColor(img, cv2.COLOR_RGBA2BGR)
         return img
                 
+    # Object detection funtion
     def object_detection(self):
         print("capturing image...")
+        # Capture image
         img = self.get_image()
-        
         # Default displacement values
         displacement_x = 0.08
         displacement_y = 0.08
-        pixel_to_meter = -0.07 #1 pixel = 0.01 meter
+        pixel_to_meter = -0.07
         
         if img is not None:
             hsv_image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+            # Establish upper and lower bounds
             lower_bound = np.array([40, 40, 40])
             upper_bound = np.array([80, 255, 255])
+            # Create mask and contours
             mask = cv2.inRange(hsv_image, lower_bound, upper_bound)
             contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
                 
             if contours:
                 print('Green object found!')
-                contour = max(contours, key=cv2.contourArea)
-                x,y,w,h = cv2.boundingRect(contour)
-                # Draw bounding box on the image
-                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)  # Green color, thickness=1
-                
-                #Calculates center of bounding box
-                center_x = x + w //2
-                center_y = y + h //2
-                
-                print("center of bounding box: ({}, {})".format(center_x, center_y))
-                
-                # Display the image with bounding box
-                cv2.circle(img, (center_x, center_y), 2, (0, 0, 255), -1)
-                cv2.imshow("Image with bounding box", img)
-                cv2.waitKey(1)  # Wait for a key press to close the window
-                cv2.destroyAllWindows()  # Close all OpenCV windows     
+                return
+            else:
+                print("no green object found")  
                            
-                
-    def rotation_search(self):
+    # Rotational search function
+    def rotational_search(self):
         print("performing rotational search...")
         # Move to the left position
         target_joint_pos_left = [0, -1.57, 1.57, -1.57, -0.6, 0.0]
         self.move_to_joint_pos(target_joint_pos_left)
         self.object_detection()
         if self.object_detection():
-        #TODO: move to position of object
-            self.grasp()
-            self.release()
+            print("Green object found!")
             # Return to the original position
             original_position = [0, -1.57, 1.57, -1.57, -1.57, 0.0]
             self.move_to_joint_pos(original_position)
@@ -177,16 +149,16 @@ class UR5e(Robot):
         self.move_to_joint_pos(target_joint_pos_right)
         self.object_detection() 
         if self.object_detection():
-        #TODO: move to position of object
-            self.grasp()
-            self.release()        
+            print("Green object found!")       
             # Return to the original position
             original_position = [0, -1.57, 1.57, -1.57, -1.57, 0.0]
             self.move_to_joint_pos(original_position)
             return
-
+            
+    # Grid search function
     def grid_search(self):
         print("implementing grid search...")
+        # Initial parameters
         x = 0.6
         y = 0.5
         z = 0.3
@@ -194,27 +166,32 @@ class UR5e(Robot):
         step_up = 0.2
         step = 3
         
+        # Move to initial position
         self.move_end_effector(x,y,z)  
         
+        # Decrease y value
         for i in range(2):
             for k in range(step):
                 y -= step_size
                 self.move_end_effector(x, y, z)
                 if self.object_detection():
                     self.grasp()
-                    return    
+                    return   
+            # Decrease x value 
             for j in range(1):
                 x += step_up
                 self.move_end_effector(x,y,z) 
                 if self.object_detection():
                     self.grasp()
-                    return       
+                    return   
+            # Increase y value    
             for m in range(step): 
                 y += step_size
                 self.move_end_effector(x,y,z)
                 if self.object_detection():
                     self.grasp()
-                    return      
+                    return     
+            # Increase x value 
             for n in range(1):
                 x += step_up
                 self.move_end_effector(x,y,z)   
@@ -222,33 +199,31 @@ class UR5e(Robot):
                     self.grasp()
                     return
 
+    # Spiral search function
     def spiral_search(self):
         print("implementing spiral search...")
-    
-        # Define initial parameters
-        x_center = 0.6  # Central x-coordinate
-        y_center = 0 # Central y-coordinate
-        z = 0.3  # Fixed z-coordinate
-        start_radius = 0.09  # Initial radius of the spiral
-        num_turns = 3 # Number of turns for the spiral
-        angle_increment = 1  # Angle increment for each step
-        radius_increment = 0.09  # Radius increment for each turn
+        # Initial parameters
+        x_center = 0.6 
+        y_center = 0
+        z = 0.3 
+        start_radius = 0.09
+        num_turns = 3
+        angle_increment = 1
+        radius_increment = 0.09
 
-        # Perform spiral search
         radius = start_radius
         for i in range(num_turns):
-            # Move along a circle with increasing radius
+            # Create circle with increasing radius
             for angle in np.arange(0, 2 * np.pi, angle_increment):
-                # Calculate x and y coordinates based on angle and radius
+                # Calculate x and y coordinates
                 x = x_center + radius * np.cos(angle)
                 y = y_center + radius * np.sin(angle)
-    
-                # Move end effector to position
+                # Move end effector to new position
                 self.move_end_effector(x, y, z)
-                #if self.object_detection():
-                 #   print("Green Object found!")
-                  #  return
-            # Increase the radius for the next turn
+                if self.object_detection():
+                    print("Green Object found!")
+                    return
+            # Increase radius for the next loop
             radius += radius_increment
     
                              
@@ -256,11 +231,6 @@ if __name__ == '__main__':
     robot = UR5e()
     robot.create_urdf('C:/Users/Nazrin/Webots_ur5eSimulation/UR5e.urdf')
     robot.move_to_joint_pos([0, -1.57, 1.57, -1.57, -1.57, 0.0])
-    #robot.grid_search()    
-    #robot.rotation_search() 
+    robot.rotational_search()     
+    robot.grid_search()    
     robot.spiral_search()   
-    #robot.object_detection()     
-    #robot.move_end_effector(0.6, 0.2, 0.0) 
-    #robot.grasp()
-    #robot.move_end_effector(0.1, 0.4, 0.7)
-    #robot.release()
